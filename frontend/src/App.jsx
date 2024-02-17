2eaa6a799baf51e0d5391c77664325c9e1984510
prashant212345 <prashant.kumar_cs22@gla.ac.in>
2024-02-17 22:32:10 +0530

    ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
   const [count, setCount] = useState(0)
-
+                   
   return (
-    <h1 className='text-3xl font-bold text-red-500 underline'>
-      hello world
-    </h1>
+    <Header />
   )
 }
 
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex items-center'>
+        Header
+    </div>
+  )
+}
+
+export default Header
