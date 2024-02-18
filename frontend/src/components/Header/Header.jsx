fa4ec93d95a18d245d7b87f02ab4c87e1f62c229
prashant212345 <prashant.kumar_cs22@gla.ac.in>
2024-02-18 15:45:34 +0530

    ok

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,19 +3,31 @@ import { IconButton } from "@material-tailwind/react";
 
 const Header = () => {
   return (
-      <div className='sticky top-0 z-50 flex items-center px-1 py-2 shadow-md bg-white'>
-        <IconButton color='blue' variant='text' size='lg'>
-          <i class="fa-solid fa-bars" />
-        </IconButton>
-        <IconButton color='blue' variant='text' size='lg'>
-          <i class="fa fa-file-text" />
-        </IconButton>
-        <h1 className='ml-2 text-gray-700 text-xl'>Docs</h1>
+    <header className='sticky top-0 z-50 flex items-center px-1 py-2 shadow-md bg-white'>
+      <IconButton color='blue' variant='text' size='lg'>
+        <i class="fa-solid fa-bars" />
+      </IconButton>
+      <IconButton color='blue' variant='text' size='lg'>
+        <i class="fa fa-file-text" />
+      </IconButton>
+      <h1 className='ml-2 text-gray-700 text-xl'>Docs</h1>
 
-        <div>
-          
-        </div>
+      <div className='mx-5 md:mx-5 flex flex-grow items-center px-5 py-2 bg-gray-100 text-gray-600 rounded-lg focus-within:text-gray-600 focus-within:shadow-md'>
+        <IconButton color='gray' variant='text' size='3xl'>
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </IconButton>
+        <input type="text" placeholder='Search' className='flex-grow px-5 text-base bg-transparent outline-none' />
+
+
       </div>
+      <IconButton color='gray' variant='text' size='md'>
+        <img src="/apps.png" alt="" />
+      </IconButton>   
+
+      <IconButton color='gray' variant='text' size='lg'>
+        <i class="fa-regular fa-circle"></i>
+      </IconButton> 
+    </header>
   )
 }
 
