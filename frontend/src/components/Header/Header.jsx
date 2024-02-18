fb4677e8192d60cd09be5855b44f03a3ca52fb3e
garvitvarshney-16 <garvitvarshney81@gmail.com>
2024-02-18 11:26:49 +0530

    header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { IconButton } from "@material-tailwind/react";
 
 const Header = () => {
   return (
-    <div className='flex items-center'>
-        Header
-    </div>
+      <div className='sticky top-0 z-50 flex items-center px-1 py-2 shadow-md bg-white'>
+        <IconButton color='blue' variant='text' size='lg'>
+          <i class="fa-solid fa-bars" />
+        </IconButton>
+        <IconButton color='blue' variant='text' size='lg'>
+          <i class="fa fa-file-text" />
+        </IconButton>
+        <h1 className='ml-2 text-gray-700 text-xl'>Docs</h1>
+
+        <div>
+          
+        </div>
+      </div>
   )
 }
 
