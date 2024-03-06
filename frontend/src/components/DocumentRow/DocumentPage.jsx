0d2d4fb5595d638359a900377ab2caca22972963
prashant212345 <prashant.kumar_cs22@gla.ac.in>
2024-03-07 01:35:17 +0530

    ok

--- frontend/src/components/DocumentRow/DocumentPage.jsx
+++ frontend/src/components/DocumentRow/DocumentPage.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import { IconButton } from "@material-tailwind/react";
+
 
 const DocumentPage = () => {
   return (
-    <div>DocumentPage</div>
+    <div>
+      <header className='flex justify-between items-center p-3 pb-1'>
+        <IconButton color='blue' variant='text' size='lg'>
+          {/* <i class="fa fa-file-text" /> */}
+          <img src="/docs.png" alt="" srcset="" />
+        </IconButton>
+
+        <div className='flex-grow px-2'>
+          <h2></h2>
+          <div className='flex items-center text-sm space-x-1 -ml-1 h-8 text-gray-600'>
+            <p className='cursor-pointer hover:bg-gray-100 transition duration-200 ease-out p-2 rounded-lg'>File</p>
+            <p className='cursor-pointer hover:bg-gray-100 transition duration-200 ease-out p-2 rounded-lg'>Edit</p>
+            <p className='cursor-pointer hover:bg-gray-100 transition duration-200 ease-out p-2 rounded-lg'>View</p>
+            <p className='cursor-pointer hover:bg-gray-100 transition duration-200 ease-out p-2 rounded-lg'>Insert</p>
+            <p className='cursor-pointer hover:bg-gray-100 transition duration-200 ease-out p-2 rounded-lg'>Format</p>
+            <p className='cursor-pointer hover:bg-gray-100 transition duration-200 ease-out p-2 rounded-lg'>Tools</p>
+          </div>
+        </div>
+
+        <button color='blue'>
+          Share
+        </button>
+
+        <img className='cursor-pointer rounded-full h-10 w-10 ml-2' src="/googleicon.png" alt="" />
+      </header>
+
+      {/* TextEditor */}
+    </div>
   )
 }
 
-export default DocumentPage
+export default DocumentPage;
+
+// export async function getServerSideProps(context) {
+//   const session = getSession(context);
+
+//   return {
+//     props:{
+
+//     }
+//   }
+// }
