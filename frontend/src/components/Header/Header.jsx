c19865e77cb376d207f1da69f7c67a25154656cc
prashant212345 <prashant.kumar_cs22@gla.ac.in>
2024-04-16 11:34:27 +0530

    Header responsive

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,6 +5,7 @@ import { IconButton, Button } from "@material-tailwind/react";
 import { useUserContext } from '../../context/userContext.jsx';
 import { useNavigate } from 'react-router-dom';
 import { SidebarWithBurgerMenu } from './SidebarWithBurgerMenu.jsx';
+import "./Header.css"
 
 const Header = () => {
   const navigate = useNavigate();
@@ -31,25 +32,26 @@ const Header = () => {
   }
 
   return (
-    <header className='sticky top-0 z-50 flex items-center px-5 py-2 shadow-md bg-white'>
+    <header className='sticky top-0 z-50 flex justify-between items-center px-5 py-2 shadow-md bg-white desktop-nav'>
+    <div className="header-left flex items-center">
       <SidebarWithBurgerMenu />
-
       <IconButton color='blue' variant='text' size='lg'>
-        <img src="/google-docs.png" alt="" srcSet='' />
+        <img src="/google-docs.png" alt="Google Docs Icon" />
       </IconButton>
       <h1 className='ml-2 text-gray-700 text-xl' style={{ fontFamily: '"Product Sans", Arial, sans-serif', color: 'gray', fontWeight: 'normal' }}>Docs</h1>
+    </div>
 
-      <div className='mx-5 md:mx-5 flex flex-grow items-center px-5 py-2 bg-gray-100 text-gray-600 rounded-lg focus-within:text-gray-600 focus-within:shadow-md' style={{ maxWidth: '1000px' }}>
-        <IconButton color='gray' variant='text' size='sm'>
-          <i className="fa-solid fa-magnifying-glass"></i>
-        </IconButton>
-        <input type="text" placeholder='Search' className='flex-grow px-5 text-base bg-transparent outline-none' />
-      </div>
-
-      <IconButton color='gray' variant='text' size='md'>
-        <img src="/apps.png" alt="" />
+    <div className='search-bar flex-grow items-center hidden md:flex mx-5 px-5 py-2 bg-gray-100 text-gray-600 rounded-lg focus-within:text-gray-600 focus-within:shadow-md'>
+      <IconButton color='gray' variant='text' size='sm'>
+        <i className="fa-solid fa-magnifying-glass"></i>
       </IconButton>
+      <input type="text" placeholder='Search' className='flex-grow px-5 text-base bg-transparent outline-none' />
+    </div>
 
+    <div className="header-right flex items-center">
+      <IconButton color='gray' variant='text' size='md'>
+        <img src="/apps.png" alt="Apps" />
+      </IconButton>
       <Button color='gray' variant='text' size='md' onClick={handleSignIn}>
         <img
           src={user?.photo || "https://docs.material-tailwind.com/icons/google.svg"}
@@ -57,7 +59,8 @@ const Header = () => {
           style={{ height: '20px', width: '20px', borderRadius: '10px' }}
         />
       </Button>
-    </header>
+    </div>
+  </header>
   );
 };
 
