cecaf2a0cff880bc28b390b4b56a092914fbc0be
garvitvarshney-16 <garvitvarshney81@gmail.com>
2024-04-19 22:11:22 +0530

    ok

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js"
 
 export const connectDB = async () => {
     try {
-        const connection = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        const connection = await mongoose.connect(process.env.MONGODB_URI)
         console.log(
             `MongoDB Connected !! DB HOST: ${connection.connection.host}`
         );
@@ -13,3 +13,5 @@ export const connectDB = async () => {
         process.exit(1);
     }
 }
+
+// `${process.env.MONGODB_URI}/${DB_NAME}`
